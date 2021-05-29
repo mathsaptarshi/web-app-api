d3833bc2fdef2120067fd1881fca2864ea4fcc72
mathsaptarshi <mathsaptarshi@gmail.com>
2021-05-30 02:22:35 +0530

    Backend First

--- house-rent/src/app/property/property-list/property-list.component.ts
+++ house-rent/src/app/property/property-list/property-list.component.ts
@@ -9,22 +9,58 @@ export class PropertyListComponent implements OnInit {
 
   properties: Array<any> = [
     {
-      Id: 1,
-      Name: "Birla",
-      Type: "House",
-      Price: 120000
+      "Id": 1,
+      "Name": "Birla",
+      "Type": "House",
+      "Price": 120000
     },
     {
-      Id: 2,
-      Name: "Exa",
-      Type: "Villa",
-      Price: 120000
+      "Id": 2,
+      "Name": "Exa",
+      "Type": "Villa",
+      "Price": 120000
     },
     {
-      Id: 3,
-      Name: "TTs",
-      Type: "House",
-      Price: 120000
+      "Id": 3,
+      "Name": "TTs",
+      "Type": "House",
+      "Price": 120000
+    },
+    {
+      "Id": 4,
+      "Name": "Exa123",
+      "Type": "Villa",
+      "Price": 120000
+    },
+    {
+      "Id": 5,
+      "Name": "TTs456",
+      "Type": "House",
+      "Price": 120000
+    },
+    {
+      "Id": 6,
+      "Name": "Exa789",
+      "Type": "Villa",
+      "Price": 120000
+    },
+    {
+      "Id": 7,
+      "Name": "TTsOOOO",
+      "Type": "House",
+      "Price": 120000
+    },
+    {
+      "Id": 8,
+      "Name": "TTs",
+      "Type": "House",
+      "Price": 120000
+    },
+    {
+      "Id": 9,
+      "Name": "YYYY",
+      "Type": "Villa",
+      "Price": 120000
     }
   ]
   constructor() { }
